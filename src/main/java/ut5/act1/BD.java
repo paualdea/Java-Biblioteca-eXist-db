509f1b253e9deea75c18b8124d8e80382bad349f
Pau Aldea <paualdeabatista@protonmail.com>
2026-04-26 09:34:06 +0200

    Creación función para crear libros.xml Closes #4

--- src/main/java/ut5/act1/BD.java
+++ src/main/java/ut5/act1/BD.java
@@ -4,12 +4,10 @@ import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
 import org.xmldb.api.base.Database;
 import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XMLResource;
 
 public class BD {
-    // Variables para la conexión a la BD
-    private String url;
-    private String usuario;
-    private String passwd;
+    private Collection coleccion;
 
     /**
      * Constructor de la clase BD.
@@ -22,10 +20,6 @@ public class BD {
      * Recibe la passwd (normalmente vacía)
      */
     public BD (String url, String usuario, String passwd) {
-        this.url = url;
-        this.usuario = usuario;
-        this.passwd = passwd;
-
         try {
             // Cargamos el driver
             Class<?> cl = Class.forName("org.exist.xmldb.DatabaseImpl");
@@ -35,16 +29,35 @@ public class BD {
             DatabaseManager.registerDatabase(database);
 
             // Configuramos la conexión a la colección raíz
-            Collection coleccion = DatabaseManager.getCollection(url, usuario, passwd);
+            coleccion = DatabaseManager.getCollection(url, usuario, passwd);
             // Creamos un objeto que sirva para administrar la DB
             CollectionManagementService administrador = (CollectionManagementService) coleccion.getService("CollectionManagementService", "1.0");
 
             // Creamos la colección principal 'Biblioteca' (si existe no se sobreescribe)
             administrador.createCollection("Biblioteca");
+            // Cambiamos la colección actual a 'Biblioteca'
+            coleccion = DatabaseManager.getCollection(url + "/Biblioteca",usuario,passwd);
         } catch (Exception e) {
             System.err.println(e.getMessage());
         }
     }
 
+    /**
+     * Función para insertar libros.xml en 'Biblioteca'.
 
+     * @param xml
+     * Recibe cómo parámetro un String que contiene xml formateado para insertar.
+     */
+    public void insertarLibros (String xml) {
+        try {
+            // Creamos un recurso en la colección llamado 'libros.xml'
+            XMLResource recurso = (XMLResource) coleccion.createResource("libros.xml", "XMLResource");
+            // Establecemos el contenido de este recurso usando la información recibida por parámetro
+            recurso.setContent(xml);
+            // Almacenamos este nuevo recurso en el servidor de DB XML
+            coleccion.storeResource(recurso);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+    }
 }
--- src/main/java/ut5/act1/Main.java
+++ src/main/java/ut5/act1/Main.java
@@ -6,9 +6,20 @@ public class Main {
         final String url = "xmldb:exist://localhost:8080/exist/xmlrpc/db";
         final String usuario = "admin", passwd = "";
 
-        // Creamos e incializamos la BD XML
+        // Creamos e inicializamos la BD XML
         BD bd = new BD(url, usuario, passwd);
 
+        // Creamos el XML estructurado para meter en libros.xml
+        String estructuraXML =
+                "<biblioteca>" +
+                "<libro>" +
+                "<titulo>Acceso a Datos</titulo>" +
+                "<autor>María López</autor>" +
+                "<precio>29.95</precio>" +
+                "</libro>" +
+                "</biblioteca>";
 
+        // Creamos libros.xml con el contenido de 'estructuraXML'
+        bd.insertarLibros(estructuraXML);
     }
 }
